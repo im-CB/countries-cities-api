628c790a0a20cae843d9f79aa43d16aed5505eb6
HariharanCB <cbhthechamp@gmail.com>
2026-07-22 16:05:57 +0530

    initial spring commit

---
+++ src/main/java/countries_cities_api/CountriesCitiesApiApplication.java
@@ -0,0 +1,13 @@
+package countries_cities_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesCitiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesCitiesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/countries_cities_api/CountriesCitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package countries_cities_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesCitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
